a42a3aef279220ddc44ea0e2087a2f9f132293c4
Glen Kutsovsky <gkstr1@hotmail.com>
2014-08-09 17:51:02 +0000

    connected listings to user_id

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,5 @@ validates_attachment_content_type :image, :content_type => ["image/jpg", "image/
    validates :name, :description, :price, presence: true
    validates :price, numericality: { greater_than: 0 }
    validates_attachment_presence :image
+    belongs_to :user
 end
