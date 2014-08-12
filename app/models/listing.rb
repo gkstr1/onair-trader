538993cf8f9e7450dfe47b75f09f05afb9db8368
Glen Kutsovsky <gkstr1@hotmail.com>
2014-08-12 01:23:45 +0000

    enable users to place orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,4 +8,5 @@ validates_attachment_content_type :image, :content_type => ["image/jpg", "image/
    validates :price, numericality: { greater_than: 0 }
    validates_attachment_presence :image
     belongs_to :user
+    has_many :orders
 end
